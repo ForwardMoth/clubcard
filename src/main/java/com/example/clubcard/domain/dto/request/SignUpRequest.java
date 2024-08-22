da4c4be30b0286aa1875fd4db366ae792349197c
ForwardMoth <Budancev-a-a1@yandex.ru>
2024-08-22 15:40:12 +0300

    update: signup dto

--- src/main/java/com/example/clubcard/domain/dto/request/SignUpRequest.java
+++ src/main/java/com/example/clubcard/domain/dto/request/SignUpRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @Schema(description = "Registration request")
@@ -16,12 +17,35 @@ public class SignUpRequest {
     private String email;
 
     @Schema(description = "Password", example = "password")
-    @Size(min=4, max=25, message = "Length of password must be no more than 25 symbols")
+    @Size(min=4, max=25, message = "Password must contain from 4 to 25 symbols")
     @NotBlank(message = "Password can't be empty")
     private String password;
 
     @Schema(description = "Confirm password", example = "password")
-    @Size(min=4, max=25, message = "Length of confirm password must be no more than 25 symbols")
+    @Size(min=4, max=25, message = "Confirm password must contain from 4 to 25 symbols")
     @NotBlank(message = "Confirm password can't be empty")
     private String confirmPassword;
+
+    @Schema(description = "First name", example = "Ivan")
+    @Size(max=50, message = "Length of first name must be no more than 50 symbols")
+    @NotBlank(message = "First name can't be empty")
+    private String firstName;
+
+    @Schema(description = "Last name", example = "Ivanov")
+    @Size(max=50, message = "Length of last name must be no more than 50 symbols")
+    @NotBlank(message = "Last name can't be empty")
+    private String lastName;
+
+    @Schema(description = "Second name", example = "Ivanovich")
+    @Size(max=50, message = "Length of second name must be no more than 50 symbols")
+    private String secondName;
+
+    @Schema(description = "Phone number", example = "81995414801")
+    @Size(min=3,max=20, message = "Phone number must contain from 3 to 20 symbols")
+    @NotBlank(message = "Phone number can't be empty")
+    private String phoneNumber;
+
+    @Schema(description = "Birthday", example = "15.08.1999")
+    @Size(max = 10, message = "Length of birthday must be no more than 11 symbols")
+    private String birthday;
 }
