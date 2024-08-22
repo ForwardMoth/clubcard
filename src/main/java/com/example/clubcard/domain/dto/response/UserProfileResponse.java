0392f9ed6927acd1186f84ff3a735c2166d5349e
ForwardMoth <Budancev-a-a1@yandex.ru>
2024-08-22 21:29:04 +0300

    add: user profile response dto

---
+++ src/main/java/com/example/clubcard/domain/dto/response/UserProfileResponse.java
@@ -0,0 +1,26 @@
+package com.example.clubcard.domain.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+@Schema(description = "User profile response")
+public class UserProfileResponse {
+    @Schema(description = "First name", example = "Ivan")
+    private String firstName;
+
+    @Schema(description = "Last name", example = "Ivanov")
+    private String lastName;
+
+    @Schema(description = "Second name", example = "Ivanovich")
+    private String secondName;
+
+    @Schema(description = "Birthday", example = "15.08.1999")
+    private String birthday;
+
+    @Schema(description = "Phone number", example = "81995414801")
+    private String phoneNumber;
+
+    @Schema(description = "Email", example = "user@mail.com")
+    private String email;
+}
