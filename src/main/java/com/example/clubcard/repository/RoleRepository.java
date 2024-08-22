7ed705bd7825a9561677769e3bcf7119af7814c9
ForwardMoth <Budancev-a-a1@yandex.ru>
2024-08-22 17:27:52 +0300

    fix: role repository

--- src/main/java/com/example/clubcard/repository/RoleRepository.java
+++ src/main/java/com/example/clubcard/repository/RoleRepository.java
@@ -4,8 +4,9 @@ import com.example.clubcard.domain.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Boolean existsByName(String name);
-    Role findByName(String name);
+    Optional<Role> findByName(String name);
 }
