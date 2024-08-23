51f4c1ba68d6d765e5aad5ca310f7b806b304ddc
ForwardMoth <Budancev-a-a1@yandex.ru>
2024-08-23 20:25:01 +0300

    add: privilege dto

--- src/main/java/com/example/clubcard/domain/dto/request/privilege/PrivilegeRequest.java
+++ src/main/java/com/example/clubcard/domain/dto/request/privilege/PrivilegeRequest.java
@@ -3,13 +3,24 @@ package com.example.clubcard.domain.dto.request.privilege;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
-@Schema(description = "Privilege request")
+@Schema(name = "Privilege request")
 public class PrivilegeRequest {
 
-    @Schema(name = "privilegeId", example = "1")
-    @NotNull(message = "Privilege id can't be empty")
-    private Long privilegeId;
+    @Schema(name = "Name", example = "standard")
+    @Size(max=50, message = "Length of name must be no more than 50 symbols")
+    @NotBlank(message = "Name can't be empty")
+    private String name;
+
+    @Schema(name = "Description", example = "standard privilege for all users")
+    @Size(max=50, message = "Length of description must be no more than 50 symbols")
+    @NotBlank(message = "Description can't be empty")
+    private String description;
+
+    @Schema(name = "Price", example = "200")
+    @NotNull(message = "Price can't be empty")
+    private Integer price;
 }
