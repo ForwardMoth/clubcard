3a70374c200a01c00dced378b1e8a6154cca8195
ForwardMoth <Budancev-a-a1@yandex.ru>
2024-08-23 20:47:44 +0300

    fix: request namings

--- src/main/java/com/example/clubcard/domain/dto/request/privilege/PrivilegeRequest.java
+++ src/main/java/com/example/clubcard/domain/dto/request/privilege/PrivilegeRequest.java
@@ -10,17 +10,17 @@ import lombok.Data;
 @Schema(name = "Privilege request")
 public class PrivilegeRequest {
 
-    @Schema(name = "Name", example = "standard")
+    @Schema(name = "name", example = "standard")
     @Size(max=50, message = "Length of name must be no more than 50 symbols")
     @NotBlank(message = "Name can't be empty")
     private String name;
 
-    @Schema(name = "Description", example = "standard privilege for all users")
+    @Schema(name = "description", example = "standard privilege for all users")
     @Size(max=50, message = "Length of description must be no more than 50 symbols")
     @NotBlank(message = "Description can't be empty")
     private String description;
 
-    @Schema(name = "Price", example = "200")
+    @Schema(name = "price", example = "200")
     @NotNull(message = "Price can't be empty")
     private Integer price;
 }
